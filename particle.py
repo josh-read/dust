a5d809ddd76073a9875283e4d2ce5aae6378b7d6
Joshua Read <joshua-read@hotmail.co.uk>
2019-11-22 23:36:16 +0000

    Add force functions

--- particle.py
+++ particle.py
@@ -2,6 +2,9 @@ from geometry import Geometry
 from vector import Vector
 from random import random
 
+G = 1
+
+
 class Particle:
 
     def __init__(self, mass, position):
@@ -11,10 +14,23 @@ class Particle:
         self.momentum = 0
 
     def two_body_force(self, other):
-        pass
+        dist_x = (other.position.x - self.position.x)
+        dist_y = (other.position.y - self.position.y)
 
-    def net_force(self, dust: dict) -> Vector:
-        f_net = 0
+        try:
+            F_x = G * self.mass * other.mass / dist_x**2
+        except ZeroDivisionError:
+            F_x = 0
+
+        try:
+            F_y = G * self.mass * other.mass / dist_y**2
+        except ZeroDivisionError:
+            F_y = 0
+
+        return Vector(F_x, F_y)
+
+    def net_force(self, dust: list) -> Vector:
+        f_net = Vector(0, 0)
         for particle in dust:
             f_net += self.two_body_force(particle)
         return f_net
