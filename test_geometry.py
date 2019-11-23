36e49d02d8b76d5dfcceae3820e12530c38d82b2
Joshua Read <josh@beans.local>
2019-11-23 05:00:33 +0000

    Finish writing geometry tests

--- test_geometry.py
+++ test_geometry.py
@@ -14,6 +14,13 @@ class TestGeometry(testing.TestCase):
 
     def test_scalar_dist(self):
         self.assertEqual(geo.scalar_dist(self.a, self.b), 5)
+        self.assertEqual(geo.scalar_dist(self.a, self.c), 3.14)
+        self.assertEqual(geo.scalar_dist(self.a, self.d), 5)
+
+    def test_collision(self):
+        self.assertTrue(geo.collision(3, self.a, 3, self.b))
+        self.assertTrue(geo.collision(2.5, self.a, 2.5, self.b))
+        self.assertFalse(geo.collision(2, self.a, 2, self.b))
 
 
 if __name__ == '__main__':
