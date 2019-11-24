37622f97b80257ddb2b29cf0d97b8b7aebe36993
Joshua Read <joshua-read@hotmail.co.uk>
2019-11-24 14:54:56 +0000

    Add command line interface to runner

--- runner.py
+++ runner.py
@@ -1,12 +1,45 @@
-from environment import Environment
-from graphics import Graphics
+import argparse
+import contextlib
 
-if __name__ == '__main__':
-    e = Environment.random_static(20)
+from environment import Environment
+with contextlib.redirect_stdout(None):
+    from graphics import Graphics
+
+
+def run_sim(env: Environment):
+    """Opens a pygame window and runs simulation.
+
+    Pygame window is initialised and updated once every 50 iterations that the
+    simulation runs. After 20000 iterations close the window closes again."""
+
     g = Graphics()
     for i in range(20000):
-        e.move()
-        e.collisions()
+        env.move()
+        env.collisions()
         if i % 50 == 0:
-            g.update(e.particles)
+            g.update(env.particles)
     g.close()
+
+
+def main():
+    """Parse command line arguments and run simulation."""
+
+    # Set up parser
+    parser = argparse.ArgumentParser(description="Run dust simulation.")
+    parser.add_argument(
+            '-n',
+            type=int,
+            default=5,
+            help="number of particles to be simulated",
+            )
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Create environment and run simulation
+    env = Environment.random_static(args.n)
+    run_sim(env)
+
+
+if __name__ == '__main__':
+    main()
