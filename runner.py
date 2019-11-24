cf9188bd32a2a7df07e9d19261efc7b239d68951
mastamysta <benjamin-read@hotmail.co.uk>
2019-11-24 14:46:35 +0000

    Added exit functionality and made loop infinite

--- runner.py
+++ runner.py
@@ -2,11 +2,17 @@ from environment import Environment
 from graphics import Graphics
 
 if __name__ == '__main__':
-    e = Environment.random_static(20)
-    g = Graphics()
-    for i in range(20000):
-        e.move()
-        e.collisions()
-        if i % 50 == 0:
-            g.update(e.particles)
-    g.close()
+    try:
+        e = Environment.random_static(15)
+        g = Graphics()
+        i = 0
+        while True:
+            i += 1
+            e.move()
+            e.collisions()
+            if i % 50 == 0:
+                i = 0
+                g.update(e.particles)
+        g.close()
+    except:
+        g.close()
