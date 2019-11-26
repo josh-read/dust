f4d5619773c1836974987dd632cf30442a73e616
mastamysta <benjamin-read@hotmail.co.uk>
2019-11-26 19:03:20 +0000

    Added full particle spawn functionality

--- dust/graphics.py
+++ dust/graphics.py
@@ -6,7 +6,7 @@ import pygame
 
 from particle import Particle
 from vector import Vector
-
+import geometry as geo
 
 class Graphics:
 
@@ -15,10 +15,12 @@ class Graphics:
         self.mouseDownPos = Vector(0, 0)
         self.mouseUpPos = Vector(0, 0)
         self.cameraPosition = Vector(0, 0)
+        self.virtualParticle = Particle(0, Vector(0, 0))
         self.zoom = 1
         self.size = width, height
         self.background = 10, 30, 43
         self.foreground = 255, 255, 255
+        self.grey = 120, 125, 121
         pygame.init()
         pygame.display.set_caption('Dust in Space')
         self.d = pygame.display.set_mode(self.size)
@@ -29,6 +31,11 @@ class Graphics:
         self.textRect.center = (100, 20)
         self.trackingCOM = False
         self.uIParticleSpawn = False
+        self.massTextBoxEditable = False
+        self.velocityTextBoxEditable = False
+        self.positionTextBoxEditable = False
+        self.momentumTextBoxEditable = False
+        self.typedInt = 0
     def close(self):
         pygame.time.delay(100)
         pygame.display.quit()
@@ -45,24 +52,17 @@ class Graphics:
 
     def update(self, ls: list):
         self.d.fill(self.background)
-        self.eventHandler()
+        self.eventHandler(ls)
         self.updateCameraPosition(ls)    
         for p in ls:
             self.drawParticle(p)        
         self.drawUI()
-        self.text = self.font.render(
-                ('%i, %i   zoom: %i %%'
-                    % (*pygame.mouse.get_pos(), self.zoom * 100)),
-                True,
-                self.foreground,
-                self.background)
-        self.d.blit(self.text, self.textRect) 
         pygame.display.update()
 
-    def eventHandler(self):
+    def eventHandler(self, ls: list):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.mouseDownHandler(event)
+                self.mouseDownHandler(event, ls)
             elif event.type == pygame.MOUSEBUTTONUP:
                 self.mouseUpHandler(event)
             elif event.type == pygame.KEYDOWN:
@@ -71,7 +71,7 @@ class Graphics:
                 self.close()
                 sys.exit()
 
-    def mouseDownHandler(self, event):
+    def mouseDownHandler(self, event, ls: list):
         if event.button == 4:
             self.zoom = self.zoom * 1.1
         elif event.button == 5:
@@ -81,7 +81,7 @@ class Graphics:
             if self.uILandingOpen:
                 self.initialUIClicks(event)
             elif self.uIParticleSpawn:
-                self.particleSpawnerClicks(event)
+                self.particleSpawnerClicks(event, ls)
             else:
                 self.noUIClicks(event)
     def initialUIClicks(self, event):
@@ -99,12 +99,24 @@ class Graphics:
             self.uILandingOpen = True
         else:
             self.dragging = True
-    def particleSpawnerClicks(self, event):
+    def particleSpawnerClicks(self, event, ls: list):
         if self.mouseDownPos.x < self.size[0] - 200:
             self.dragging = True
         elif self.withinRect(pygame.Rect((self.size[0] - 95, 420),(80,40))):
             self.uIParticleSpawn = False
             self.uILandingOpen = True
+            self.virtualParticle = Particle(0, Vector(0,0))
+        elif self.withinRect(pygame.Rect((self.size[0] - 190, 220),(175, 35))):
+            self.massTextBoxEditable = True
+        elif self.withinRect(pygame.Rect((self.size[0] - 190, 265),(175, 40))):
+            self.positionTextBoxEditable = True
+        elif self.withinRect(pygame.Rect((self.size[0] - 190, 320),(175, 35))):
+            self.velocityTextBoxEditable = True
+        elif self.withinRect(pygame.Rect((self.size[0] - 190, 365),(175, 35))):
+            self.momentumTextBoxEditable = True
+        elif self.withinRect(pygame.Rect((self.size[0] - 190, 420),(90, 40))):
+            ls.append(self.virtualParticle)
+            self.virtualParticle = Particle(0, Vector(0,0))
     def withinRect(self, r: pygame.Rect):
         if self.mouseDownPos.x > r.left and self.mouseDownPos.y > r.top \
         and self.mouseDownPos.x < r.left + r.width and self.mouseDownPos.y < r.top + r.height:
@@ -119,11 +131,56 @@ class Graphics:
 
     def keyDownHandler(self, event):
         if event.key == pygame.K_ESCAPE:
-            self.close()
-            sys.exit()
+            self.typedInt = 0
+            self.massTextBoxEditable = False
+            self.velocityTextBoxEditable = False
+            self.positionTextBoxEditable = False
+            self.momentumTextBoxEditable = False
         elif event.key == pygame.K_1:
-            pass
-
+            self.typedInt = self.typedInt * 10 + 1
+        elif event.key == pygame.K_2:
+            self.typedInt = self.typedInt * 10 + 2   
+        elif event.key == pygame.K_3:
+            self.typedInt = self.typedInt * 10 + 3
+        elif event.key == pygame.K_4:
+            self.typedInt = self.typedInt * 10 + 4
+        elif event.key == pygame.K_5:
+            self.typedInt = self.typedInt * 10 + 5
+        elif event.key == pygame.K_6:
+            self.typedInt = self.typedInt * 10 + 6
+        elif event.key == pygame.K_7:
+            self.typedInt = self.typedInt * 10 + 7
+        elif event.key == pygame.K_8:
+            self.typedInt = self.typedInt * 10 + 8
+        elif event.key == pygame.K_9:
+            self.typedInt = self.typedInt * 10 + 9
+        elif event.key == pygame.K_0:
+            self.typedInt = self.typedInt * 10
+        
+        if event.key == pygame.K_RETURN:
+            if self.massTextBoxEditable == True:
+                self.massTextBoxEditable = False
+                self.virtualParticle.mass = self.typedInt
+                try:
+                    self.virtualParticle.radius = geo.radius_from_mass(self.virtualParticle.mass)
+                except ValueError:  # Raised when negative mass is used
+                    self.virtualParticle.radius = 0
+                self.typedInt = 0
+            if self.velocityTextBoxEditable == True:
+                self.velocityTextBoxEditable = False
+                self.virtualParticle.velocity.x = self.typedInt
+                self.virtualParticle.velocity.y = self.typedInt
+                self.typedInt = 0
+            if self.positionTextBoxEditable == True:
+                self.positionTextBoxEditable = False
+                self.virtualParticle.position.x = self.typedInt
+                self.virtualParticle.position.y = self.typedInt
+                self.typedInt = 0
+            if self.momentumTextBoxEditable == True:
+                self.momentumTextBoxEditable = False
+                self.virtualParticle.momentum.x = self.typedInt
+                self.virtualParticle.momentum.y = self.typedInt
+                self.typedInt = 0
     def updateCameraPosition(self, ls: list):
         if self.dragging:
             self.cameraPosition = self.cameraPosition + (self.mouseDownPos - Vector(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])) * (1 / self.zoom) 
@@ -132,15 +189,41 @@ class Graphics:
             self.cameraPosition = sum(ls).position - Vector((self.size[0] - 200)/2, self.size[1]/2)
 
     def drawUI(self):
+        self.text = self.font.render(
+                ('%i, %i   zoom: %i %%'
+                    % (*pygame.mouse.get_pos(), self.zoom * 100)),
+                True,
+                self.foreground,
+                self.background)
+        self.d.blit(self.text, self.textRect) 
         currentdir = os.path.dirname(os.path.realpath(__file__))
         if self.uILandingOpen:
             path = os.path.join(currentdir, "UserInterface/uILanding.jpg")
             UI = pygame.image.load(path)
-            self.d.blit(UI, (self.size[0] - 200, 0))
-        elif self.uIParticleSpawn:
+            self.d.blit(UI, (self.size[0] - 200, 0))           
+        elif self.uIParticleSpawn:            
             path = os.path.join(currentdir, "UserInterface/particleSpawner.jpg")
             UI = pygame.image.load(path)
             self.d.blit(UI, (self.size[0] - 200, 0))
+            t = self.font.render(('Mass: %i' % (self.virtualParticle.mass)), True, self.foreground, self.grey)
+            massTextBox = t.get_rect()
+            massTextBox.center = (900, 240)         
+            self.d.blit(t, massTextBox)
+            
+            t = self.font.render('Position - x:%i  y:%i' % (self.virtualParticle.position.x, self.virtualParticle.position.y), True, self.foreground, self.grey)
+            massTextBox = t.get_rect()
+            massTextBox.center = (900, 290)
+            self.d.blit(t, massTextBox)
+            
+            t = self.font.render('Velocity - x:%i  y:%i' % (self.virtualParticle.velocity.x, self.virtualParticle.velocity.y), True, self.foreground, self.grey)
+            massTextBox = t.get_rect()
+            massTextBox.center = (900, 340)
+            self.d.blit(t, massTextBox)
+            
+            t = self.font.render('Momentum - x:%i  y:%i' % (self.virtualParticle.momentum.x, self.virtualParticle.momentum.y), True, self.foreground, self.grey)
+            massTextBox = t.get_rect()
+            massTextBox.center = (900, 390)
+            self.d.blit(t, massTextBox)
         else:
             path = os.path.join(currentdir, "UserInterface/openSettings.jpg")
             UI = pygame.image.load(path)
