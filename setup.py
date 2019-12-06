e624ce0a5a311f9c6f63ae03660dd482c0b9daad
Joshua Read <josh@beans.lan>
2019-12-06 11:04:35 +0000

    Fill out setup.py and add extension modules field

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name="dust", packages=find_packages())
+from Cython.Build import cythonize
+
+setup(
+    name='Dust',
+    description="Particle dynamics simulation/visualisation built on Pygame.",
+    author='Joshua Read, Benjamin Read',
+    packages=find_packages(),
+    # ext_modules=cythonize("example.pyx")
+    )
